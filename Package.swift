75aa38ac6da2da10e3a60834a04ec4477812c1a9
Rahim <rahim.mohamed@acsicorp.com>
2025-05-22 14:45:48 +0530

    version added

--- Package.swift
+++ Package.swift
@@ -14,6 +14,9 @@ let package = Package(
             name: "AlignedCollectionViewFlowLayout",
             targets: ["AlignedCollectionViewFlowLayout"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/RahimPasha1/AligneCollectionView.git", from: "1.0.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
